acc0bc28e1225a0d48af7db54ae5d7be578d2e88
Magishetty Saikumar <saimagishetty01@gmail.com>
2023-10-11 20:34:03 +0530

    updatr

--- src/app/components/ims/ims.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ims',
-  templateUrl: './ims.component.html',
-  styleUrls: ['./ims.component.scss']
-})
-export class ImsComponent {
-}
